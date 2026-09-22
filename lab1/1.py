64d34de511e2527eeb93ed62c45f23c6e95849c0
Roman <suhihroman28@gmail.com>
2026-09-22 12:49:05 +1000

    feat: start lab 1

---
+++ lab1/1.py
@@ -0,0 +1,11 @@
+# 1
+
+station_name = "Ocean"
+active_sensors = 18
+average_temperature = 21.6
+is_online = True
+
+print(station_name, type(station_name))
+print(active_sensors, type(active_sensors))
+print(average_temperature, type(average_temperature))
+print(is_online, type(is_online))
